33e37595fda548f8ce0cc03039bd179779cc28d8
a-faraz <anjumfaraz10@gmail.com>
2017-03-06 19:05:05 -0600

    update portfolio

--- src/common/components/About.js
+++ src/common/components/About.js
@@ -25,18 +25,17 @@ class About extends Component {
 
       	  	<h3>About Me</h3>
 
-      	  	<p>I grew up in Houston Texas and attended the University of Houston from 2010-2014. I graduated with a Bachelors in Managment Information Systems.</p>
+      	  	<p>I grew up in Houston Texas and attended the University of Houston, where I graduated with a Bachelors in Managment Information Systems.</p>
 
-      	  	<p>During my college career, I enrolled in many programming and business classes. I was fortunate enough to partake in internships which helped me develop a broad skillset.</p>
+            <p>From helping schools in the village where I was born to spending hours in the Senior Living Home by my house in Houston, I was fortunate enough to partake in various programs that helped me become the person I am today.</p>
 
-            <p>At my first internship, as a Corporate Development Intern for the Houston Rockets, I learned to utilize my business and communication skills.</p>
-            <p>My second internhip was with The Citizens Foundation, a Non-Profit Organization, where I utilized agile methodologies to develop software.</p>
+            <p>Taking my first programming class in college was one of the hardest things I had to overcome. The challenge sparked an interest that would go on to create a fiery passion for software development.</p>
 
       	  	<p>After graduating, I worked as a Business Analyst. I was working face to face with clients and handling finances for my branch.</p>
-            <p>Meanwhile, I started my own company, a clothing line, where I handled everything from design, to marketing, to the e-commerce.</p>
-            <p>Afterwards, I worked as a Systems Analyst for a healthcare administration company, where I was responsible for three clients. I handled their employees' benefits' data and web applications.</p>
-            <p>I love software development because it not only gives me the chance to express my creativity but it also allows me to make a difference in people's lives.</p>
-      	    <p>The idea of creating solutions for real world problems is what intrigues me most.</p>
+            <p>Meanwhile, I started my second company, a clothing line, where I handled everything from design, to marketing, to the e-commerce.</p>
+            <p>Afterwards, I worked as a Systems Analyst for a healthcare administration company, where I was responsible for three clients. I handled their employee benefits data and web applications.</p>
+            <p>I am passionate about software development because it not only gives me the chance to express my creativity but also represents a tool which I can use to help others.</p>
+      	    <p>The idea of creating solutions for real world problems is my biggest motivation right after making my parents proud.</p>
 	      </div>
 
 	      <div className="repos">
