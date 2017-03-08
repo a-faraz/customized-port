762d035d943a28b543e823fd7950356de0cbb983
a-faraz <anjumfaraz10@gmail.com>
2017-03-08 10:41:46 -0600

    update

--- src/common/components/About.js
+++ src/common/components/About.js
@@ -25,17 +25,14 @@ class About extends Component {
 
       	  	<h3>About Me</h3>
 
-      	  	<p>I grew up in Houston Texas and attended the University of Houston, where I graduated with a Bachelors in Managment Information Systems.</p>
+      	  	<p>I grew up in Texas and attended the University of Houston, where I graduated with a Bachelors in Managment Information Systems.</p>
 
-            <p>From helping schools in the village where I was born to spending hours in the Senior Living Home by my house in Houston, I was fortunate enough to partake in various programs that helped me become the person I am today.</p>
+            <p>I took my first programming class in college and it was one of the hardest things I had to overcome. The challenge sparked a curiosity that would go on to create a passion for software development.</p>
 
-            <p>Taking my first programming class in college was one of the hardest things I had to overcome. The challenge sparked an interest that would go on to create a fiery passion for software development.</p>
-
-      	  	<p>After graduating, I worked as a Business Analyst. I was working face to face with clients and handling finances for my branch.</p>
-            <p>Meanwhile, I started my second company, a clothing line, where I handled everything from design, to marketing, to the e-commerce.</p>
-            <p>Afterwards, I worked as a Systems Analyst for a healthcare administration company, where I was responsible for three clients. I handled their employee benefits data and web applications.</p>
-            <p>I am passionate about software development because it not only gives me the chance to express my creativity but also represents a tool which I can use to help others.</p>
-      	    <p>The idea of creating solutions for real world problems is my biggest motivation right after making my parents proud.</p>
+            <p>Also while in college, I started my second company, a clothing line, where I handled everything from design, to marketing, to the e-commerce.</p>
+            <p>After college, I worked as a Systems Analyst for a healthcare administration company. There I was responsible for three clients. I handled their employee benefits data and web applications.</p>
+            <p>Coming from IT, I have worked hard to develop a solid understanding of computer programming funadamentals and best practices. Programming not only gives me pleasure and satisfaction but also represents a tool which I can use to help others.</p>
+      	    <p>The idea of creating solutions for real world problems is my biggest motivation.</p>
 	      </div>
 
 	      <div className="repos">
